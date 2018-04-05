c2bd6659b6f4fbc6572bc3c989f76a76393cddcb
jimmy90321 <jimmy4302001@gmail.com>
2018-04-05 15:26:41 +0800

    add progressdialog

--- app/src/main/java/com/app/jlin/cafetraveler/Activity/IntroActivity.java
+++ app/src/main/java/com/app/jlin/cafetraveler/Activity/IntroActivity.java
@@ -1,5 +1,6 @@
 package com.app.jlin.cafetraveler.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -33,16 +34,25 @@ import okhttp3.Response;
  * Created by stanley.lin on 2018/3/31.
  */
 
-public class IntroActivity extends BaseActivity{
+public class IntroActivity extends BaseActivity {
 
-    private Handler handler ;
+    private Handler handler;
+    private ProgressHandler progressHandler;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        mProgressDialog = new ProgressDialog(IntroActivity.this);
         handler = new Handler();
+        progressHandler = new ProgressHandler(mProgressDialog);
+
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setMessage("資料更新中：");
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.show();
 
         OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder()
@@ -65,19 +75,20 @@ public class IntroActivity extends BaseActivity{
     private Callback callback = new Callback() {
         @Override
         public void onFailure(Call call, IOException e) {
-            Log.e("onFailure",e.toString());
+            Log.e("onFailure", e.toString());
         }
 
         @Override
         public void onResponse(Call call, Response response) throws IOException {
             String responseBody = response.body().string();
 
-            JsonArray jsonElements = new Gson().fromJson(responseBody,JsonArray.class);
-            Log.e("jsonElements",String.valueOf(jsonElements.size()));
-            Log.e("RMCafe.getAll",String.valueOf(RMCafe.getAll().size()));
+            final JsonArray jsonElements = new Gson().fromJson(responseBody, JsonArray.class);
+            Log.e("jsonElements", String.valueOf(jsonElements.size()));
+            Log.e("RMCafe.getAll", String.valueOf(RMCafe.getAll().size()));
 
             //資料數量不一樣在做處理
-            if(jsonElements.size() != RMCafe.getAll().size()){
+            if (jsonElements.size() != RMCafe.getAll().size()) {
+                progressHandler.setTotal(jsonElements.size());
                 RMCafe.deleteAll();
                 ArrayList<RMCafe> rmCafeArrayList = new ArrayList<>();
                 for (int i = 0; i < jsonElements.size(); i++) {
@@ -97,43 +108,76 @@ public class IntroActivity extends BaseActivity{
                     }
                     rmCafe.setMyMrt(finalMrt);
                     rmCafeArrayList.add(rmCafe);
-                    LogUtils.e("for",String.valueOf(rmCafeArrayList.size()));
+                    progressHandler.setProgress(i);
+                    progressHandler.sendEmptyMessage(0);
+                    LogUtils.e("for", String.valueOf(rmCafeArrayList.size()));
                 }
                 RMCafe.addAll(rmCafeArrayList);
             }
 
+            progressHandler.disDialog();
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    startActivity(new Intent(IntroActivity.this,MainActivity.class));
+                    startActivity(new Intent(IntroActivity.this, MainActivity.class));
                     finish();
                 }
-            },1000);
+            }, 1000);
         }
     };
 
     /**
      * 取得json file內所有捷運站的List
-     * */
-    private List<MrtModel> getMrtJsonList(){
+     */
+    private List<MrtModel> getMrtJsonList() {
         List<MrtModel> mrtList = new ArrayList<>();
 
         InputStream is = null;
-        try{
+        try {
             is = getAssets().open("Mrt.json");
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            String mrtData = new String(buffer,"UTF-8");
+            String mrtData = new String(buffer, "UTF-8");
             JSONArray mrtJsonArray = new JSONArray(mrtData);
-            for(int i = 0 ; i < mrtJsonArray.length() ; i++){
-                MrtModel mrtModel = new Gson().fromJson(mrtJsonArray.get(i).toString(),MrtModel.class);
+            for (int i = 0; i < mrtJsonArray.length(); i++) {
+                MrtModel mrtModel = new Gson().fromJson(mrtJsonArray.get(i).toString(), MrtModel.class);
                 mrtList.add(mrtModel);
             }
-        }catch (IOException | JSONException e){
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
         return mrtList;
+    }
+
+    private class ProgressHandler extends Handler {
+        private ProgressDialog mProgressDialog;
+        private int progress,total;
+
+        public ProgressHandler(ProgressDialog mProgressDialog) {
+            this.mProgressDialog = mProgressDialog;
+        }
+
+        public void setTotal(int total){
+            this.total = total;
+        }
+
+        public void setProgress(int progress){
+            this.progress = progress;
+            updateDialog();
+        }
+
+        public void disDialog(){
+            mProgressDialog.dismiss();
+        }
+
+        private void updateDialog(){
+            if(progress == total){
+                mProgressDialog.dismiss();
+            }else{
+                mProgressDialog.setProgress((int)(progress*1.0f/total*100));
+            }
+        }
     }
 }
